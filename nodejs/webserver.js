d0e529e5a48cde746d0fbe6c4f294a2d5937f149
johnivarhauge <35365734+johnivarhauge@users.noreply.github.com>
2018-01-29 20:37:54 +0100

    Update webserver.js

--- nodejs/webserver.js
+++ nodejs/webserver.js
@@ -14,35 +14,82 @@ db.serialize(function() {
     db.run("CREATE TABLE IF NOT EXISTS Dikt (diktID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,dikt TEXT NOT NULL)");
 });
 
-//Login service: for å registre og logge inn bruker
-restapi.get('/bruker/:id', function(req, res){
-    db.get("SELECT * FROM Bruker where brukerID = ?",[req.params.id], function(err, row){
+//Login service: 
+//for å sjekke om bruker finnes. 
+restapi.get('/brukersjekk/:brukernavn', function(req, res){
+    db.get("SELECT count(*) FROM Bruker where brukernavn = ?",[req.params.brukernavn], function(err, row){
         if (err){
-            console.err(err);urn:MyData
+            console.err(err);
             res.status(500);
         }
         else {
             res.set('Content-Type', 'application/xml');
-            var xmlstring = '<?xml version="1.0"?>\n<bruker xmlns="https://www.w3schools.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="brukerschema.xsd">' + jsontoxml(row) + '</bruker>' 
+            var xmlstring = '<?xml version="1.0"?>\n<Antall xmlns="https://www.w3schools.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="brukerschema.xsd">' + jsontoxml(row) + '</Antall>' 
             //lage bruker xml skjemaurn:MyData
             res.send(xmlstring);
-            console.log(req.body);
         }
     });
 });
-
-restapi.post('/bruker/:brukernavn:passordhash:passord', function(req, res){
-    db.run("INSERT INTO BRUKER(brukernavn, passordhash, passord) VALUES(?,?,?)",[req.params.brukernavn,req.params.passordhash,req.params.passord], function(err, row){
+//hente ut passordhash 
+restapi.get('/passordsjekk/:brukernavn', function(req, res){
+    db.get("SELECT passordhash FROM Bruker where brukernavn = ?",[req.params.brukernavn], function(err, row){
         if (err){
-            console.err(err);urn:MyData
+            console.err(err);
             res.status(500);
         }
         else {
             res.set('Content-Type', 'application/xml');
-            var xmlstring = '<?xml version="1.0"?>\n<bruker xmlns="https://www.w3schools.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="brukerschema.xsd">' + jsontoxml(row) + '</bruker>' 
-            //lage bruker xml skjemaurn:MyData
+            var xmlstring = '<?xml version="1.0"?>\n<Passordhash xmlns="https://www.w3schools.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="brukerschema.xsd">' + jsontoxml(row) + '</Passordhash>' 
             res.send(xmlstring);
-            console.log(req.body);
+        }
+    });
+});
+//registrere ny bruker
+restapi.post('/nybruker/:brukernavn/:passordhash/:passord', function(req, res){
+    db.run("INSERT INTO BRUKER(brukernavn, passordhash, passord) VALUES((?),(?),(?))",[req.params.brukernavn,req.params.passordhash,req.params.passord], function(err, row){
+        if (err){
+            console.err(err);
+            res.status(500);
+        }
+        else {
+            console.log('ny bruker opprettet');
+        }
+    });
+});
+ //sjekke bruker id
+ restapi.get('/brukerid/:brukernavn', function(req, res){
+    db.get("SELECT brukerID FROM Bruker where brukernavn = (?)",[req.params.brukernavn], function(err, row){
+        if (err){
+            console.err(err);
+            res.status(500);
+        }
+        else {
+            res.set('Content-Type', 'application/xml');
+            var xmlstring = '<?xml version="1.0"?>\n<BrukerID xmlns="https://www.w3schools.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="brukerschema.xsd">' + jsontoxml(row) + '</BrukerID>' 
+            res.send(xmlstring);
+        }
+    });
+ }); 
+//Opprette ny sesjon returnerer sesjonsid
+ restapi.get('/nysesjon/:brukerID', function(req, res){
+    db.run("INSERT INTO Sesjon(brukerID) VALUES(?)",[req.params.brukerID],function(err, row){
+        if (err){
+            console.err(err);
+            res.status(500);
+        }
+        else {
+            console.log('ny bruker opprettet');
+        }
+    });
+    db.get("SELECT sesjonsID FROM Sesjon where brukernavn = (row)", function(err, row){
+        if (err){
+            console.err(err);
+            res.status(500);
+        }
+        else {
+            res.set('Content-Type', 'application/xml');
+            var xmlstring = '<?xml version="1.0"?>\n<SesjonsID xmlns="https://www.w3schools.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="brukerschema.xsd">' + jsontoxml(row) + '</SesjonsID>' 
+            res.send(xmlstring);
         }
     });
 });
@@ -50,7 +97,7 @@ restapi.post('/bruker/:brukernavn:passordhash:passord', function(req, res){
 restapi.get('/data/:id', function(req, res){
     db.get("SELECT * FROM Bruker where brukerID = ?",[req.params.id], function(err, row){
         if (err){
-            console.err(err);urn:MyData
+            console.err(err);
             res.status(500);
         }
         else {
@@ -66,7 +113,7 @@ restapi.get('/data/:id', function(req, res){
 restapi.get('/data/', function(req, res){
     db.get("SELECT * FROM Bruker", function(err, row){
         if (err){
-            console.err(err);urn:MyData
+            console.err(err);
             res.status(500);
         }
         else {

10e1a5778c53d9c6f8f461a0acb6f305f7f320c8
kreutzz <adrian93kreutz@gmail.com>
2018-01-29 14:12:07 +0100

    changed testAPI.sh

--- nodejs/testAPI.sh
+++ nodejs/testAPI.sh
@@ -1,16 +1,16 @@
 
 echo get
-curl --request GET -D- http://localhost:3000/data
+curl --request GET -D- http://localhost:3000/data/1
 read -n 1 -s -r -p "Press any key to continue" 
 
 echo post
 curl --request POST --header "Content-type: text/xml" \
---data '{"brukerID": 10, }' -D- http://localhost:3000/data
+-F test=@/home/nan/danan/danan/nodejs/adduser.xml -D- http://localhost:3000/data
 read -n 1 -s -r -p "Press any key to continue" 
 
 echo delete
 curl --request DELETE --header "Content-type: text/xml" \
---data '{"brukerID": 2, }' -D- http://localhost:3000/data
+--data '{ }' -D- http://localhost:3000/data
 read -n 1 -s -r -p "Press any key to continue" 
 
 echo put

82e7e95a294850799f8ab25fc88bd83661af0989
johnivarhauge <johnivar.hauge@gmail.com>
2018-01-29 01:44:28 -0800

    webtjener

--- nodejs/webserver.js
+++ nodejs/webserver.js
@@ -9,21 +9,19 @@ db.serialize(function() {
     db.run("PRAGMA foreign_keys = 1");
     db.run("CREATE TABLE IF NOT EXISTS Bruker (brukerID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,passordhash TEXT NOT NULL)");
     db.run("CREATE TABLE IF NOT EXISTS Sesjon (sesjonsID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,brukerID INTEGER NOT NULL, FOREIGN KEY(brukerID) REFERENCES Bruker(brukerID))");
+    db.run("CREATE TABLE IF NOT EXISTS Dikt (diktID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,dikt TEXT NOT NULL)");
 });
 
-restapi.get('/data', function(req, res){
-    db.get("SELECT * FROM Bruker", function(err, row){
+restapi.get('/data/:id', function(req, res){
+    db.get("SELECT * FROM Bruker where brukerID = ?",[req.params.id], function(err, row){
         if (err){
             console.err(err);
             res.status(500);
         }
         else {
-            res.send(row);
+            res.set('Content-Type', 'text/xml');
+            res.send(xml(row)));
         }
-        
-        /*var xmlString = xml([{ count: row.value }]);
-		res.set('Content-Type', 'text/xml');
-		res.send(xmlString);*/
     });
 });
 

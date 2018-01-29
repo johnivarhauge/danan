b805f72149419b3b4c5aa2bcf328e28d4124b03c
kreutzz <adrian93kreutz@gmail.com>
2018-01-29 11:54:13 +0100

    la til mulighet for å få tilbake respons header

--- nodejs/testAPI.sh
+++ nodejs/testAPI.sh
@@ -1,18 +1,18 @@
 
 echo get
-curl --request GET http://localhost:3000/data
+curl --request GET -D- http://localhost:3000/data
 read -n 1 -s -r -p "Press any key to continue" 
 
 echo post
 curl --request POST --header "Content-type: text/xml" \
---data '{"brukerID": 10, }' http://localhost:3000/data
+--data '{"brukerID": 10, }' -D- http://localhost:3000/data
 read -n 1 -s -r -p "Press any key to continue" 
 
 echo delete
 curl --request DELETE --header "Content-type: text/xml" \
---data '{"brukerID": 2, }' http://localhost:3000/data
+--data '{"brukerID": 2, }' -D- http://localhost:3000/data
 read -n 1 -s -r -p "Press any key to continue" 
 
 echo put
 curl --request PUT --header "Content-type: text/xml" \
---data '{}' http://localhost:3000/data
+--data '{}' -D- http://localhost:3000/data
